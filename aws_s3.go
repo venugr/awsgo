5a869573fde0994713802d379d2e8dd7e85b12af
Lella, Venugopala <Venugopala.Lella@ellucian.com>
2021-04-08 16:30:19 +0530

    added s3 calls for list buckets/objects and put tagging

--- aws_s3.go
+++ aws_s3.go
@@ -2,13 +2,113 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"io"
 	"log"
+	"os"
+	"strings"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/aws/aws-sdk-go-v2/service/s3/types"
 )
 
 func DoS3(myConfig aws.Config, myContext context.Context) {
+
+	//*******************************************************************************
+	bucketName := "myaudiomp3"
+	isOk, isErr := DoPutBucketTagging(myConfig, myContext, bucketName)
+
+	if isErr != nil || !isOk {
+		log.Fatalf("Error: unable to put tagging: %v", isErr)
+	}
+
+}
+
+func DoS3_2(myConfig aws.Config, myContext context.Context) {
+
+	//*******************************************************************************
+	buckets, isOk, isErr := DoListBuckets(myConfig, myContext)
+
+	if isErr != nil || !isOk {
+		log.Fatalf("Error: unable to list buckets: %v", isErr)
+	}
+
+	log.Println("---------------------------------")
+	log.Println("\t\tBuckets")
+	log.Println("---------------------------------")
+	cnt := 1
+	for _, bucket := range strings.Split(buckets, ",") {
+		if bucket == "" {
+			continue
+		}
+		log.Printf("%v.%v", cnt, bucket)
+		cnt++
+	}
+
+	if cnt == 1 {
+		log.Printf("<No Buckets>")
+	}
+
+	log.Println("---------------------------------")
+
+	//*******************************************************************************
+	for _, bucketName := range strings.Split(buckets, ",") {
+
+		if bucketName == "" {
+			continue
+		}
+
+		objects, isOk, isErr := DoListObjects(myConfig, myContext, bucketName)
+
+		if isErr != nil || !isOk {
+			log.Println()
+			log.Printf("Error: unable to list objects in bucket '%v': %v", bucketName, isErr)
+			log.Println()
+			continue
+		}
+
+		log.Println()
+		log.Println("---------------------------------")
+		log.Printf("\tObjects in '%v'\n", bucketName)
+		log.Println("---------------------------------")
+		cnt = 1
+		for _, object := range strings.Split(objects, ",") {
+			if object == "" {
+				continue
+			}
+			log.Printf("%v.%v", cnt, object)
+			cnt++
+		}
+
+		if cnt == 1 {
+			log.Printf("<No Objects>")
+		}
+
+		log.Println("---------------------------------")
+		log.Println()
+	}
+
+}
+
+func DoS3_1(myConfig aws.Config, myContext context.Context) {
+
+	//*******************************************************************************
+	getBucket := "myaudiomp3"
+	getKey := "London.mp3"
+
+	objReader, isOk, isErr := DoGetObject(myConfig, myContext, getBucket, getKey)
+
+	if isErr != nil || !isOk {
+		log.Fatalf("Error: unable to get object '%v': %v", getKey, isErr)
+	}
+
+	log.Printf("Info: body: %v", objReader)
+
+	outFile, isErr := os.Create("/tmp/my_" + getKey)
+	_, isErr = io.Copy(outFile, objReader)
+
+	return
 
 	//*******************************************************************************
 	bucketName := "mymestbucketdellme"
@@ -31,11 +131,9 @@ func DoS3(myConfig aws.Config, myContext context.Context) {
 
 	log.Printf("Info: object '%v' is copied to as '%v'", srcObject, key)
 
-	DoSleep(20, "deleting object...")
-
 	//*******************************************************************************
 	keyPutObj := "putobject.mp3"
-	isOk, isErr = DoPutObject(myConfig, myContext, bucketName, keyPutObj)
+	isOk, isErr = DoPutObject(myConfig, myContext, bucketName, keyPutObj, strings.NewReader("My text for Put Object"))
 
 	if isErr != nil || !isOk {
 		log.Fatalf("Error: unable to put object '%v': %v", keyPutObj, isErr)
@@ -225,14 +323,175 @@ func GoPutObject(myConfig aws.Config, myContext context.Context, params *s3.PutO
 	return s3Resp, nil
 
 }
-func DoPutObject(myConfig aws.Config, myContext context.Context, bucketName string, key string) (bool, error) {
+func DoPutObject(myConfig aws.Config, myContext context.Context, bucketName string, key string, srcData io.Reader) (bool, error) {
 
 	s3Input := &s3.PutObjectInput{
 		Bucket: &bucketName,
 		Key:    &key,
+		Body:   srcData,
 	}
 
 	_, isErr := GoPutObject(myConfig, myContext, s3Input)
+
+	if isErr != nil {
+		return false, isErr
+	}
+
+	return true, nil
+}
+
+func GoGetObject(myConfig aws.Config, myContext context.Context, params *s3.GetObjectInput) (*s3.GetObjectOutput, error) {
+
+	s3Client := s3.NewFromConfig(myConfig)
+
+	if params == nil {
+		params = &s3.GetObjectInput{}
+	}
+
+	s3Resp, isErr := s3Client.GetObject(myContext, params)
+
+	if isErr != nil {
+		return nil, isErr
+	}
+
+	return s3Resp, nil
+
+}
+func DoGetObject(myConfig aws.Config, myContext context.Context, bucketName string, key string) (io.ReadCloser, bool, error) {
+
+	s3Input := &s3.GetObjectInput{
+		Bucket: &bucketName,
+		Key:    &key,
+	}
+
+	outResp, isErr := GoGetObject(myConfig, myContext, s3Input)
+
+	if isErr != nil {
+		return nil, false, isErr
+	}
+
+	return outResp.Body, true, nil
+}
+
+func GoListBuckets(myConfig aws.Config, myContext context.Context, params *s3.ListBucketsInput) (*s3.ListBucketsOutput, error) {
+
+	s3Client := s3.NewFromConfig(myConfig)
+
+	if params == nil {
+		params = &s3.ListBucketsInput{}
+	}
+
+	s3Resp, isErr := s3Client.ListBuckets(myContext, params)
+
+	if isErr != nil {
+		return nil, isErr
+	}
+
+	return s3Resp, nil
+
+}
+func DoListBuckets(myConfig aws.Config, myContext context.Context) (string, bool, error) {
+
+	s3Input := &s3.ListBucketsInput{}
+
+	outResp, isErr := GoListBuckets(myConfig, myContext, s3Input)
+
+	if isErr != nil {
+		return "", false, isErr
+	}
+
+	buckets := ""
+
+	for _, bucket := range outResp.Buckets {
+
+		buckets += *bucket.Name + ","
+	}
+
+	return buckets, true, nil
+}
+
+func GoListObjects(myConfig aws.Config, myContext context.Context, params *s3.ListObjectsInput) (*s3.ListObjectsOutput, error) {
+
+	s3Client := s3.NewFromConfig(myConfig)
+
+	if params == nil {
+		params = &s3.ListObjectsInput{}
+	}
+
+	s3Resp, isErr := s3Client.ListObjects(myContext, params)
+
+	if isErr != nil {
+		return nil, isErr
+	}
+
+	return s3Resp, nil
+
+}
+func DoListObjects(myConfig aws.Config, myContext context.Context, bucketName string) (string, bool, error) {
+
+	s3Input := &s3.ListObjectsInput{
+		Bucket: &bucketName,
+	}
+
+	outResp, isErr := GoListObjects(myConfig, myContext, s3Input)
+
+	if isErr != nil {
+		return "", false, isErr
+	}
+
+	objects := ""
+
+	for _, content := range outResp.Contents {
+		objects += *content.Key + "(size:" + fmt.Sprint(content.Size) + "),"
+	}
+
+	return objects, true, nil
+}
+
+func GoPutBucketTagging(myConfig aws.Config, myContext context.Context, params *s3.PutBucketTaggingInput) (*s3.PutBucketTaggingOutput, error) {
+
+	s3Client := s3.NewFromConfig(myConfig)
+
+	if params == nil {
+		params = &s3.PutBucketTaggingInput{}
+	}
+
+	s3Resp, isErr := s3Client.PutBucketTagging(myContext, params)
+
+	if isErr != nil {
+		return nil, isErr
+	}
+
+	return s3Resp, nil
+
+}
+func DoPutBucketTagging(myConfig aws.Config, myContext context.Context, bucketName string) (bool, error) {
+
+	s3Input := &s3.PutBucketTaggingInput{
+		Bucket: &bucketName,
+		Tagging: &types.Tagging{
+			TagSet: []types.Tag{
+				{
+					Key:   aws.String(string("testTagName")),
+					Value: aws.String(string("Name1")),
+				},
+				{
+					Key:   aws.String(string("Name")),
+					Value: aws.String(string("Audio")),
+				},
+				{
+					Key:   aws.String(string("Type")),
+					Value: aws.String(string("MP3")),
+				},
+				{
+					Key:   aws.String(string("DND")),
+					Value: aws.String(string("FALSE")),
+				},
+			},
+		},
+	}
+
+	_, isErr := GoPutBucketTagging(myConfig, myContext, s3Input)
 
 	if isErr != nil {
 		return false, isErr
